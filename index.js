d1bd4e43f9c4b54483f493ac28cf9267395ddda6
christinezosche <christine.zosche@gmail.com>
2020-10-25 08:12:32 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+function writeCards(names, occasion) {
+    const messages = [];
+    for (let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${occasion} gift!`);
+
+    }
+   
+    return messages;
+  }
+
+function countDown(number) {
+
+  let countdown = number;
+  
+    while (countdown >= 0) {
+        console.log(countdown--);
+    }
+}
