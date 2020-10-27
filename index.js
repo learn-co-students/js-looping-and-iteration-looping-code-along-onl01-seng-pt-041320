d85de850eb187302a7f65e90950881393f874cb9
Dan Ste. Marie Jr <DSte.mariejr@gmail.com>
2020-10-26 22:17:56 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(recipients, message) {
+    const messages = []
+    for (let i = 0; i < recipients.length; i++) {
+        messages.push(`Thank you, ${recipients[i]}, for the wonderful ${message} gift!`) ;
+    }
+
+    return messages;
+}
+
+function countDown(start) {
+    while (start >= 0)  {
+        console.log(start--);
+    }
+}
