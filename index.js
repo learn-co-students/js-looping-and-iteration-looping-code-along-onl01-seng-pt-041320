5e9e45df1ae9a928327a7bf8e8e81a50212f0747
github usernameIfeAkiWad <github email addressifeoluwa.wade@gmail.com>
2020-11-01 18:07:40 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solutions in this file
+
+const names = ['Lisa', 'Kaitlin', 'Jan'],
+    array = [ ]
+    x = 'surprise';
+
+function writeCards(names, x) {
+    for (let i = 0; i < names.length; i++) {
+       array.push(`Thank you, ${names[i]}, for the wonderful ${x} gift!`);
+    }
+    return array;
+}
+
+writeCards(names, x);
+
+// function countDown() {
+//    let i = 11
+//    while (i > 0) {
+//        console.log(`${i}`);
+//        i--
+//     }
+//   }
+function countDown (i) {
+    while (i >= 0){
+        console.log(i);
+        i--;
+    }
+}
