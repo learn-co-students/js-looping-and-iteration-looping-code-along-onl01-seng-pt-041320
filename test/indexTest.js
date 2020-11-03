3094784bd286dea5c201a0866708062448494996
timothyphilip <philippetimothe@gmail.com>
2020-11-02 23:10:12 -0500

    Done.

--- test/indexTest.js
+++ test/indexTest.js
@@ -30,18 +30,18 @@ describe( 'index.js', () => {
         .to.eq( 11 );
     } );
 
-    // it( 'logs each number when counting down, starting from the number provided', () => {
-    //   countDown( 4 );
-    //   expect( spy.calledWithExactly( 4 ), "Expected countDown(4) to log 4 first" )
-    //     .to.be.true;
-    //   expect( spy.calledWithExactly( 3 ), "Expected countDown(4) to log 3 after 4" )
-    //     .to.be.true;
-    //   expect( spy.calledWithExactly( 2 ), "Expected countDown(4) to log 2 after 3" )
-    //     .to.be.true;
-    //   expect( spy.calledWithExactly( 1 ), "Expected countDown(4) to log 1 after 2" )
-    //     .to.be.true;
-    //   expect( spy.calledWithExactly( 0 ), "Expected countDown(4) to log 0 after 1" )
-    //     .to.be.true;
-    // } );
+    it( 'logs each number when counting down, starting from the number provided', () => {
+      countDown( 4 );
+      expect( spy.calledWithExactly( 4 ), "Expected countDown(4) to log 4 first" )
+        .to.be.true;
+      expect( spy.calledWithExactly( 3 ), "Expected countDown(4) to log 3 after 4" )
+        .to.be.true;
+      expect( spy.calledWithExactly( 2 ), "Expected countDown(4) to log 2 after 3" )
+        .to.be.true;
+      expect( spy.calledWithExactly( 1 ), "Expected countDown(4) to log 1 after 2" )
+        .to.be.true;
+      expect( spy.calledWithExactly( 0 ), "Expected countDown(4) to log 0 after 1" )
+        .to.be.true;
+    } );
   } );
 } );
