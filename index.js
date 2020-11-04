dfd3ac6229bb9e4411b35e9b8f33a941ee3f774b
Jgamez17 <jpgamez1789@icloud.com>
2020-11-04 15:08:52 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+
+const names = ['Lisa', 'Kaitlin', 'Jan'];
+const event = 'birthday';
+
+function writeCards(names, event)  {
+    const messages = [];
+    for(let i=0; i<names.length; i++){
+        messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+   return messages;
+};
+
+function countDown (i) {
+    while (i >= 0){
+        console.log(i);
+        i--;
+    }
+}
+    
